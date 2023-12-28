35d3687ff3e13460d29c8824818fa3b54dfbe17c
Igor Osipov <ighor.osipov.95@mail.ru>
2023-12-28 10:37:33 +0300

    Update project.

---
+++ app/src/main/java/ighorosipov/dimberchat/presentation/ui/screens/grouplist/GroupList.kt
@@ -0,0 +1,14 @@
+package ighorosipov.dimberchat.presentation.ui.screens.grouplist
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun GroupList(
+    paddingValues: PaddingValues,
+    viewModel: GroupListViewModel = hiltViewModel(),
+    onNavigate: (String) -> Unit
+) {
+
+}
---
+++ app/src/main/java/ighorosipov/dimberchat/presentation/ui/screens/grouplist/GroupListViewModel.kt
@@ -0,0 +1,10 @@
+package ighorosipov.dimberchat.presentation.ui.screens.grouplist
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GroupListViewModel @Inject constructor(): ViewModel() {
+    
+}
